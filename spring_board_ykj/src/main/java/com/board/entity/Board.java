3d74d34cde5207bd656003ca25d669c81b9debb6
kyungjae-Y <yoonkj2008@gmail.com>
2024-03-12 18:40:55 +0900

    2024.03.12
    2024.03.12

---
+++ spring_board_ykj/src/main/java/com/board/entity/Board.java
@@ -0,0 +1,13 @@
+package com.board.entity;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	private int idx;
+	private String title;
+	private String content;
+	private String writer;
+	private String indate;
+	private int count;
+}
